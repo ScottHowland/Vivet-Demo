893a9b03ed1ac023dd7c60cb97dd022339e94413
WaveForceTyphoon <Showland17@my.whitworth.edu>
2015-02-26 16:13:48 -0800

    Initial Unreal Commit

---
+++ VivetDemo/Source/VivetDemo/VivetDemoGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "VivetDemoGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VIVETDEMO_API AVivetDemoGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
